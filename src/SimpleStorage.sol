682f35e0456ea63bd324a88c7ba73ce0ee5c518f
Raghavendra-Havale <raghavendra.havale12@gmail.com>
2023-07-03 15:34:18 +0530

    simple storage created

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,19 +1,15 @@
-// I'm a comment!
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.8;
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.8;
 
 contract SimpleStorage {
-
     uint256 favoriteNumber;
 
     struct People {
         uint256 favoriteNumber;
         string name;
     }
-    // uint256[] public anArray;
+
     People[] public people;
 
     mapping(string => uint256) public nameToFavoriteNumber;
@@ -21,8 +17,8 @@ contract SimpleStorage {
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
-    
-    function retrieve() public view returns (uint256){
+
+    function retrieve() public view returns (uint256) {
         return favoriteNumber;
     }
 
